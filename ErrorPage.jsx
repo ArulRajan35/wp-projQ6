51433cc33a55fcebc15faa98b39e49c1324a8c29
ArulRajan35 <230701035@rajalakshmi.edu.in>
2025-10-31 00:12:19 +0530

    Create ErrorPage.jsx

---
+++ ErrorPage.jsx
@@ -0,0 +1,12 @@
+Src//Components//ErrorPage.jsx 
+ 
+import React from 'react'; 
+ 
+const ErrorPage = () => ( 
+  <div> 
+    <h1>Something Went Wrong</h1> 
+    <p>We're sorry, but an error occurred. Please try again later.</p> 
+  </div> 
+); 
+ 
+export default ErrorPage; 
